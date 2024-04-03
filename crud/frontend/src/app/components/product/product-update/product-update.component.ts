190ac9d06985c0a21ade089b192256948905ac6a
Wilson Junior <wilson.assis.junior@gmail.com>
2024-04-03 17:20:40 -0300

    Componente atualizar produto

---
+++ crud/frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrl: './product-update.component.css'
+})
+export class ProductUpdateComponent implements OnInit {
+
+  product!: Product;
+
+  constructor(private productService: ProductService,
+              private router: Router,
+              private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id !== null) {
+      this.productService.readById(id).subscribe(prod => {
+        this.product = prod;
+      });
+    } else {
+      console.error("ID não encontrado na rota.");
+    }
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}

dcfb6cd57f7e507e857e5358fd924d96692dcc98
Wilson Junior <wilson.assis.junior@gmail.com>
2024-04-09 21:24:47 -0300

    Atualizando títulos

--- crud/frontend/src/app/views/home/home.component.ts
+++ crud/frontend/src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderService } from '../../components/template/header/header.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  
+  constructor(private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Início',
+      icon: 'home',
+      routeUrl: ''
+    }
+  }
 }
--- crud/frontend/src/app/views/product-crud/product-crud.component.ts
+++ crud/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from '../../components/template/header/header.service';
 
 @Component({
   selector: 'app-product-crud',
@@ -8,7 +9,14 @@ import { Router } from '@angular/router';
 })
 export class ProductCrudComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
+  }
 
   navigateToProductCreate() : void {
     this.router.navigate(['/products/create'])
